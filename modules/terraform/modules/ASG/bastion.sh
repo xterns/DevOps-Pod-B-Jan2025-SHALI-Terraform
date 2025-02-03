5190e130b09194cc31a3acd72509fd03d5df8ae4
Akindiya Olalekan <lehqandiya@gmail.com>
2025-02-03 10:09:10 +0100

    first commit on this shali project updated (#9)
    * first commit on this shali project updated
    
    * commiting CHANGELOG to to the repository
    
    * added updated packer ami
    
    * added terraform-project to replace module in folder
    
    * corrected the IAM policy to latest:2012-10-17
    
    * changed description
    
    * added the vpc_cidr to var.tf file
    
    * added main.tf to terraform
    
    * added main.tf to terraform
    
    * added mamodule/ami-ec2_instance/main.tf to main.tf
    
    * added second commit to the repo
    
    * added ami_id to repo
    
    * Delete modules directory
    
    The modules directory was deleted because the directed was a replica
    
    * Delete module directory
    
    another replica of module directory deleted

--- modules/terraform/modules/ASG/bastion.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo yum install mysql -y
-sudo yum install git tmux
-sudo yum install ansible -y
---
+++ terraform_project/module/ASG/bastion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#sudo yum install mysql -y
+#sudo yum install git -y,
+#sudo yum install ansible -y
