ad5262aed28ac3be83c69c2487ffc570562bc730
Akindiya Olalekan <lehqandiya@gmail.com>
2025-02-14 13:53:53 +0100

    Delete modules directory

--- modules/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-pod-b-jan2025-shali-terraform-s3" # Replace with your bucket name
-    key            = "terraform/terraform.tfstate"             # Replace with a custom path
-    region         = "us-east-1"                               # Replace with your bucket region
-    dynamodb_table = "devops-pod-b-jan2025-shali-terraform-db" # Replace with your DynamoDB table name
-    encrypt        = true                                      # Enable encryption for added security
-  }
-}
