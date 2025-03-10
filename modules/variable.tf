f7345a43552153a9c60265fd4ed77c7af147983b
Olalekan Akindiya <lehqandiya@gmail.com>
2025-03-10 18:25:04 +0100

    added profile for each aws account provider to provision EC2 on different evvironments

--- modules/variable.tf
+++ modules/variable.tf
@@ -1,3 +1,9 @@
+variable "aws_profile" {
+  description = "AWS CLI profile name (optional)"
+  type        = string
+  default     = "default"
+}
+
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
@@ -29,9 +35,9 @@ variable "environment" {
   type        = string
 }
 
-variable "aws_region" {
-  description = "AWS region for resources"
-  type        = string
-  default     = "us-east-1"
-}
+# variable "aws_region" {
+#   description = "AWS region for resources"
+#   type        = string
+#   default     = "us-east-1"
+# }
 
